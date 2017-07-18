3a8640f1a4e31ccb59c4c3291ba7d94a9d8779be
imranh654 <umemous@gmail.com>
2017-07-18 14:06:03 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function get_child_pages_list_function() {
+	$current_repair_page_id = get_the_ID();
+
+	$gtchldpgsargs = array(
+		'sort_column' => 'post_date',
+		'sort_order' => 'DESC',
+		'parent' => $current_repair_page_id,
+		'post_type' => 'page',
+		'post_status' => 'publish'
+	); 
+
+	$getchildpages = get_pages($gtchldpgsargs);
+
+	$repair_child_page_cnt .='<ul class="simple">';
+
+	foreach( $getchildpages as $getchildpages ) { 
+		
+		$repair_child_page_cnt .= '<li class="item-'.$getchildpages->ID.'"><a class="chilpagelink" href="'.get_permalink($getchildpages->ID).'" rel="bookmark" title="'.$getchildpages->post_title.'">'. $getchildpages->post_title .'</a></li>';
+	}
+		
+	$repair_child_page_cnt .='</ul>';
+	return $repair_child_page_cnt;
+}
+add_shortcode('get-child-pages-list', 'get_child_pages_list_function');
